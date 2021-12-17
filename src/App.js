0c9e1669bf8ec71f16e2c363ecb3e8224eb6019f
NewtonJoaquim <newtonjoaquim12@gmail.com>
2021-12-17 00:48:06 -0300

    Criada base da página de dashboards

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import {  Routes,Route } from 'react-router-dom';
-import { Login } from './pages';
+import { Login, Home } from './pages';
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Login/> }/>
+      <Route path='/' element={<Login /> }/>
+      <Route path='/dashboard' element={<Home /> }/>
     </Routes>
   );
 }
