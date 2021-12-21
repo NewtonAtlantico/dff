19f3f20e32604cadc65b360f76f8ee1213f1c3f8
Newton Neto <newton.neto@hp.com>
2021-12-21 10:41:36 -0300

    Integrar com dashboards do superset

--- src/pages/Dashboard/dashboard.jsx
+++ src/pages/Dashboard/dashboard.jsx
@@ -3,9 +3,14 @@ import React from 'react';
 const Dashboard = ({dashboardName}) => {
     
     return (
-        <h2>
-            {dashboardName}
-        </h2>
+        <iframe
+      width='100%'
+      height='95vh'
+      style={{minHeight:'90vh'}}
+      frameBorder="0"
+      src={`http://localhost:8088/login?username=admin&redirect=/superset/dashboard/${dashboardName}/?standalone=true`}
+    >
+    </iframe>
     )
 }
 
